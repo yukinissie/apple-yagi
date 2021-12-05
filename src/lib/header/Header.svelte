8288e474212a3b994235a828d0a501020711fc8b
apple-yagi <applesryu@gmail.com>
2021-12-05 20:17:52 +0900

    Add adapter-static

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 	import logo from './svelte-logo.svg';
 </script>
 
@@ -15,9 +16,13 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
+			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="{base}/">Home</a></li>
+			<li class:active={$page.path === '/about'}>
+				<a sveltekit:prefetch href="{base}/about">About</a>
+			</li>
+			<li class:active={$page.path === '/todos'}>
+				<a sveltekit:prefetch href="{base}/todos">Todos</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
